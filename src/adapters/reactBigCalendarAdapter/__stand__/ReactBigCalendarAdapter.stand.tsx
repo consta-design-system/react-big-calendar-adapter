7e5196d5d38773b133d734b12c6d4464b47718c8
Maxim Kolesnikov <44449561+N1MBER@users.noreply.github.com>
2022-08-31 14:52:03 +0300

    docs(documentation): add doc for react-big-calendar stand (#6)
    consta-design-system/uikit#2568

--- src/adapters/reactBigCalendarAdapter/__stand__/ReactBigCalendarAdapter.stand.tsx
+++ src/adapters/reactBigCalendarAdapter/__stand__/ReactBigCalendarAdapter.stand.tsx
@@ -4,7 +4,9 @@ export default createStand({
   title: 'reactBigCalendarAdapter',
   id: 'reactBigCalendarAdapter',
   group: 'adapters',
-  description: 'Показывает загрузку файла или уже загруженный файл.',
+  description: 'Адаптер для стилизации календаря.',
   version: '0.1.1',
+  figma:
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D1336%253A68963',
   status: 'stable',
 });
