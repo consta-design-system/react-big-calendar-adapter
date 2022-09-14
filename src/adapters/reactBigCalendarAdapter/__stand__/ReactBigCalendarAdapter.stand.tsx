cf8197bcb3c0c466b69c9fc876bf531fb0f993f5
N1MBER <socialfox33@gmail.com>
2022-09-14 17:35:37 +0300

    fix(sandbox): rewrite links to ids

--- src/adapters/reactBigCalendarAdapter/__stand__/ReactBigCalendarAdapter.stand.tsx
+++ src/adapters/reactBigCalendarAdapter/__stand__/ReactBigCalendarAdapter.stand.tsx
@@ -6,8 +6,7 @@ export default createStand({
   group: 'adapters',
   description: 'Адаптер для стилизации календаря.',
   version: '0.1.1',
-  sandbox:
-    'https://codesandbox.io/embed/reactbigcalendaradapter-12f2pk?fontsize=14&hidenavigation=1&theme=dark',
+  sandbox: 'reactbigcalendaradapter-12f2pk',
   figma:
     'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FLKYyjgQ3khFHvXraZAG1n1%2FConsta-Components%3Fnode-id%3D1336%253A68963',
   status: 'stable',
